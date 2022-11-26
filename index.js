ff97977e9bd828febe1ff2f1b4dfdb2bf3695f9c
GladysKigen <gladys.kigen@student.moringa.com>
2022-11-26 22:06:56 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// code your solution here
+// code your solution 
+//const ages = [3, 10, 18, 20];
+
+//function checkAge(age) {
+ // return age > 18;
+//}
+
+//function myFunction() {
+ // document.getElementById("demo").innerHTML = ages.find(checkAge);
+//} 
+function superbowlWin(record){
+    let game =record.find((game)=>{
+        if(game.result==='W'){
+            return true;
+        }
+    })
+
+if(game){
+    return game.year
+}
+}
